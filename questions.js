1bd4db1c4647282fc4073120ce7782b23c563a1c
kishore-kilaru34 <78412196+kishore-kilaru34@users.noreply.github.com>
2021-06-18 10:29:12 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the 16-bit compiler allowable range for integer constants?",
+    answer: "-32768 to 32767",
+    options: [
+      "-3.4e38 to 3.4e38",
+      "-32767 to 32768",
+      "-32668 to 32667",
+      "-32768 to 32767"
+    ]
+  },
+    {
+    numb: 2,
+    question: " What is required in each C program?",
+    answer: "The program must have at least one function.",
+    options: [
+      "The program must have at least one function.",
+      "The program does not require any function.",
+      "Input data",
+      "Output data"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following comment is correct when a macro definition includes arguments?",
+    answer: "The opening parenthesis should immediately follow the macro name.",
+    options: [
+      "The opening parenthesis should immediately follow the macro name.",
+      "There should be at least one blank between the macro name and the opening parenthesis.",
+      "There should be only one blank between the macro name and the opening parenthesis.",
+      "All the above comments are correct."
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is a lint?",
+    answer: "Analyzing tool",
+    options: [
+      "C compiler",
+      "Interactive debugger",
+      "Analyzing tool",
+      "C interpreter"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Why is a macro used in place of a function? ",
+     answer: "It increases code size.",
+     options: [
+      "It reduces execution time.",
+      "It reduces code size.",
+      "It increases execution time.",
+      "It increases code size."
+     ]
+
+  },
+  {  
+    numb: 6,
+    question: "In the C language, the constant is defined _______.",
+    answer: "Anywhere, but starting on a new line.",
+    options: [
+      "Before main",
+      "After main",
+      "Anywhere, but starting on a new line.",
+      "None of the these."
+
+    ]
+
+  }, 
+   {
+     numb: 7,
+     question:"If abcdefg is the input, the output will be",
+     answer:"efg",
+     options: [
+      "abcd",
+      "abc",
+      "efg",
+      "Garbage"
+     ]
+   },
+   {
+    numb: 8,
+    question:"A pointer is a memory address. Suppose the pointer variable has p address 1000, and that p is declared to have type int*, and an int is 4 bytes long. What address is represented by expression p + 2?",
+    answer:"1008",
+    options: [
+      "1002",
+      "1004",
+      "1006",
+      "1008"
+    ]
+    },
+    {
+      numb: 9,
+    question:" Which one of the following is a loop construct that will always be executed once?",
+    answer:"do while",
+    options: [
+      "for",
+      "while",
+      "switch",
+      "do while"
+    ]
+    },
+    {
+      numb: 10,
+      question:" Which of the following best describes the ordering of destructor calls for stack-resident objects in a routine?",
+      answer:"The first object destroyed is the last object destroyed; last created is first destroyed.",
+      options: [
+        "The first object created is the first object destroyed; last created is last destroyed.",
+        "The first object destroyed is the last object destroyed; last created is first destroyed.",
+        "Objects are destroyed in the order they appear in memory, the object with the lowest memory address is destroyed first.",
+        "The order is undefined and may vary from compiler to compiler."
+      ]
+      }
+];
